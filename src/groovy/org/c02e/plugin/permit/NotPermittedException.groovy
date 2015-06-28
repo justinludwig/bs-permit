931b1a9c715fb5839009719192e194f778484e39
Justin Ludwig <justinludwig@gmail.com>
2015-06-28 12:39:13 -0700

    clean whitespace

--- src/groovy/org/c02e/plugin/permit/NotPermittedException.groovy
+++ src/groovy/org/c02e/plugin/permit/NotPermittedException.groovy
@@ -1,19 +1,19 @@
 package org.c02e.plugin.permit
 
 class NotPermittedException extends Exception {
-    
+
     NotPermittedException() {
         super()
     }
-    
+
     NotPermittedException(String message) {
         super(message)
     }
-    
+
     NotPermittedException(String message, Throwable cause) {
         super(message, cause)
     }
-    
+
     NotPermittedException(Throwable cause) {
         super(cause)
     }
